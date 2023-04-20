6e7c3c71b043b3245e94a09f43d55838c003f31e
stasiaDiamond <nsisco@uw.edu>
2023-04-19 22:39:07 -0700

    more whitespace fixes for md

---
+++ Develop/GeneratedREADME.md
@@ -0,0 +1,36 @@
+# asdasdasd
+
+## Description
+
+sad
+
+## Table of Contents
+
+asd
+
+## Installation
+
+asd
+
+## Usage
+
+asd
+
+## License
+
+MIT 
+
+## Contributing
+
+as
+
+## Tests
+
+d
+
+## Questions
+
+Link to my GitHub: <https://github.com/sad>
+
+For additional questions, please email: ds
+        
--- Develop/index.js
+++ Develop/index.js
@@ -63,65 +63,64 @@ inquirer
         console.log(ans);
         const output = `# ${ans.projectTitle}
 
-        ## Description
+## Description
 
-        ${ans.description}
+${ans.description}
 
-        ## Table of Contents
+## Table of Contents
 
-        ${ans.tableContents}
+${ans.tableContents}
 
-        ## Installation
+## Installation
 
-        ${ans.installation}
+${ans.installation}
 
-        ## Usage
+## Usage
 
-        ${ans.usage}
+${ans.usage}
 
-        ## License
+## License
 
-        ${ans.license} 
-        
-        ## Contributing
+${ans.license} 
 
-        ${ans.contributing}
+## Contributing
 
-        ## Tests
+${ans.contributing}
 
-        ${ans.tests}
+## Tests
 
-        ## Questions
-        
-        Link to my GitHub: <https://github.com/${ans.questionGitUserName}>
-        For additional questions, email: ${ans.questionEmail}
-        `
-        console.log(output)
-    })
+${ans.tests}
+
+## Questions
+
+Link to my GitHub: <https://github.com/${ans.questionGitUserName}>
+
+For additional questions, please email: [Contact Email](mailto:${ans.questionEmail})
+`;
+        console.log(output);
+
+        fs.writeFile("GeneratedREADME.md", output, err => {
+            if(err){
+                console.log(err);
+            } else {
+                console.log("Your README has been generated.");
+            }
+        });
+    });
+    
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
 
 
 
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
